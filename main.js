5a57e4900fbf68a013ac1850ae14ffaecf05f9e3
J9pro <janine239@gmail.com>
2022-12-20 19:40:21 +0100

    third

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
 const score = document.querySelector('.score');
 const resetBtn = document.querySelector("#reset");
-const numbers = document.querySelector(".nums")
 const choose = document.getElementById("choose-num");
+const player1Btn = document.querySelector("#a");
+const player2Btn = document.querySelector("#b")
 
 
 //GET VALUE FROM DROPDOWN & SET VALUE IN SCORE-BOARD
 choose.addEventListener('change', () => {
-    let selected = Number(choose.options[choose.selectedIndex].value);
-    //console.log(selected)
-
+    // Number(choose.options[choose.selectedIndex].value);
+    choose.value
 });
 
 
-//LOG WINS TO Score-Board
-const player1Btn = document.querySelector("#a");
-const player2Btn = document.querySelector("#b") //HERE!!!
-//console.log(player2Btn, "player2")
+
+
 
 let start = 0;
 const resultR = document.getElementById("red-score");
 player1Btn.addEventListener("click", () => {
     start++;
-    start = (start <= 11) ? "0" + start : start;
-    resultR.innerHTML = start;
-
-    // score.innerHTML = "0";
+    Number(resultR.innerHTML = start);
+    if (Number(start) == choose.value) {
+        alert("YOU WIN!!")
+        resultR.style.color = "limegreen";
+    } else {
+        resultR.style.color = "red";
+    }
 });
 
 let startG = 0;
 const result = document.getElementById("green-score");
 player2Btn.addEventListener("click", () => {
     startG++;
-    if (startG <= 11) {
-        console.log(startG)
-    } else {
-        console.log("nope")
-    }
-
     Number(result.innerHTML = startG);
-    //console.log(typeof +startG)
+    if (Number(startG) == choose.value) {
+        //console.log("hello world")
+        result.style.color = "limegreen";
+    } else {
+        //console.log("sorry")
+        result.style.color = "red";
+    }
 
 });
 
@@ -55,6 +56,8 @@ resetBtn.addEventListener("click", () => {
         score.style.color = "white";
         document.querySelector("#green-score").style.color = "white";
         document.querySelector("#green-score").innerHTML = "0";
+        startG = 0
+        start = 0
 
 
     }
@@ -65,19 +68,5 @@ resetBtn.addEventListener("click", () => {
 //THINGS THE GAME NEEDS
 ////////////////////////
 
-//needS a place to show each player's points?
-
-//function PLAYER LOSES
-
-//PLAYER WINS
-// if (player1Wins === numbers) {
-//     console.log("perfect")
-// } else {
-//     console.log("nope")
-// }
-
-
-//CHECKS IF VALUES ARE EQUAL
-// const isEqual = (objG, obj2) => {
-
-// }
+//change winning/ losing colors
+//make a winning condition
