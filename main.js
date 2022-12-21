aaaabe4a9bf9ff7a2cc2c5bd71aba38071645007
J9pro <janine239@gmail.com>
2022-12-21 16:24:34 +0100

    fourth

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ const player2Btn = document.querySelector("#b")
 
 
 //GET VALUE FROM DROPDOWN & SET VALUE IN SCORE-BOARD
-choose.addEventListener('change', () => {
+choose.addEventListener('change', function () {
     // Number(choose.options[choose.selectedIndex].value);
     choose.value
+    score.innerHTML = "0";
+    score.style.color = "white";
+    document.querySelector("#green-score").style.color = "white";
+    document.querySelector("#green-score").innerHTML = "0";
+    startG = 0
+    start = 0
+    // clearedBtn();
 });
-
-
-
 
 
 let start = 0;
@@ -21,11 +25,19 @@ player1Btn.addEventListener("click", () => {
     start++;
     Number(resultR.innerHTML = start);
     if (Number(start) == choose.value) {
-        alert("YOU WIN!!")
         resultR.style.color = "limegreen";
-    } else {
-        resultR.style.color = "red";
+        console.log("YOU WIN!!")
+        start = 0;
+        startG = 0;
+        result.style.color = "red";
+
+    } else if (Number(start) < choose.value) {
+        resultR.style.color = "white";
+
     }
+    //  else {
+    //     resultR.style.color = "red";
+    // }
 });
 
 let startG = 0;
@@ -34,21 +46,28 @@ player2Btn.addEventListener("click", () => {
     startG++;
     Number(result.innerHTML = startG);
     if (Number(startG) == choose.value) {
-        //console.log("hello world")
+        console.log("hello world")
         result.style.color = "limegreen";
-    } else {
+        startG = 0;
+        start = 0;
+        resultR.style.color = "red";
+    } else if (Number(startG) < choose.value) {
         //console.log("sorry")
-        result.style.color = "red";
+        result.style.color = "white";
+
     }
+    // else {
+    //     //console.log("sorry")
+    //     result.style.color = "red";
+
+    // }
 
 });
 
 
 
-
 //RESET BUTTON
-resetBtn.addEventListener("click", () => {
-
+resetBtn.addEventListener("click", function () {
     // clears every cell
     for (let i = 0; i <= 11; i++) {
         // i.innerHTML = "";
@@ -68,5 +87,10 @@ resetBtn.addEventListener("click", () => {
 //THINGS THE GAME NEEDS
 ////////////////////////
 
-//change winning/ losing colors
-//make a winning condition
+//when player wins- you should not be able to continue incrementing
+
+//whenever you change the selected target- everything should reset.
+
+//clean up IDs in html
+
+//increase the width of button's size
